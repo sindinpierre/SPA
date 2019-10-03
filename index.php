e6e29423b6d5e32b75335f00c21e0a3a0d6d9a6b
Sindin Pierre <pierre.sindin@gmail.com>
2019-10-03 21:23:31 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="content">
+        <div id="hero">
+        <input type="radio" id="i1" name="videos" checked />
+        <input type="radio" id="i2" name="videos" />
+        <input type="radio" id="i3" name="videos" />
+
+            <div class="slide_videos" id="one">
+                <iframe class="videoFrames" width="100%" height="500" src="https://www.youtube.com/embed/5ZR1MgQWwCE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <label class="prev" for="i3"><span>&#x2039;</span></label>
+                <label class="next" for="i2"><span>&#x203a;</span></label>
+            </div>
+
+            <div class="slide_videos" id="two">
+
+                <iframe class="videoFrames" width="100%" height="100%" src="https://www.youtube.com/embed/I0_Qv8wCRaE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <label class="prev" for="i1"><span>&#x2039;</span></label>
+                <label class="next" for="i3"><span>&#x203a;</span></label>
+
+            </div>
+
+            <div class="slide_videos" id="three">
+                <iframe class="videoFrames" width="100%" height="100%" src="https://www.youtube.com/embed/SnIrvrTx7FA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <label class="prev" for="i2"><span>&#x2039;</span></label>
+                <label class="next" for="i1"><span>&#x203a;</span></label>
+            </div>
+            <div id="nav_slide">
+                <label for="i1" class="dots" id="dot1"></label>
+                <label for="i2" class="dots" id="dot2"></label>
+                <label for="i3" class="dots" id="dot3"></label>
+            </div>
+        </div>
+        <div id="services" style="font-size: 150%;">
+        <p><b style="font-weight: bold;">qu'est ce que BFM TV ?</b><br/>
+        <br/>
+        BFM TV est une chaîne de télévision française d'information nationale en continu, filiale du groupe NextRadioTV, qui détient entre autres BFM Business, RMC Story, RMC Découverte, BFM Paris.<br/>
+        <br/>
+        Elle est librement accessible, principalement sur la TNT (sauf outre-mer), le câble, le satellite, la télévision par ADSL (IPTV), la télévision mobile personnelle (sur smartphones et tablettes) et en lecture en continu sur Internet.<br/>
+        <br/>
+        Créée le 28 novembre 2005, elle est directement lancée sur la TNT alors naissante. La chaîne iTélé (renommée depuis CNews), filiale du groupe Canal+, déjà existante et arrivant en même temps sur la TNT, devient alors sa principale concurrente.<br/>
+        <br/>
+        Marc-Olivier Fogiel en est le directeur général depuis juillet 2019, succédant à Hervé Beroud désormais directeur général délégué d'Altice Média2.<br/>
+        <br/>
+        Le sigle BFM signifie Business FM le nom de la radio d'information financière BFM dont elle devait être la déclinaison télé. </p></div>
+
+        <div id="ourTeam">
+            <div id="teamPictures">
+                <div id="imageProfile1"><img src="https://img.bfmtv.com/c/1256/708/d95/a5321183e045d0ed7b4ebea1ee6d2.png" alt=""></div>
+                <div id="imageProfile2"><img src="https://pbs.twimg.com/profile_images/585698235727097856/F5OgcgIO_400x400.jpg" alt=""></div>
+                <div id="imageProfile3"><img src="https://img.bfmtv.com/c/400/200/6f5/6f5fd14012bdbce11b4c8c8a9dc51bdc.jpeg" alt=""></div>
+
+            </div>
+            <div id="teamDescription">
+                <div id="descriptionProfile1" style="font-size: 150%;"><p>Bruno Le Maire,<br/>
+                <br/>  
+                né le 15 avril 1969 à Neuilly-sur-Seine, est un homme politique français. Normalien et énarque, il est directeur de cabinet du Premier ministre Dominique de Villepin entre 2006 et 2007 puis est élu député UMP dans l'Eure à cette date.</p></div>
+
+                <div id="descriptionProfile2" style="font-size: 150%;"><p>Jean-Jacques Bourdin,<br/> 
+                <br/> 
+                né à Colombes le 15 juin 1949, est un journaliste et animateur de radio et de télévision français. Depuis 2001, il est l'animateur de l'émission matinale Bourdin Direct sur RMC.</p></div>
+
+                <div id="descriptionProfile3" style="font-size: 150%;"><p>Nicolas Doze,<br/>
+                <br/> 
+                né le 25 avril 1969 à Boulogne Billancourt, est un éditorialiste radio et télé français spécialisé en économie. Après avoir travaillé à Radio Classique, il travaille depuis 2001 sur les chaînes du groupe NextRadioTV : BFM TV, RMC et BFM Business.</p></div>
+
+            </div>
+        </div>
+        <div id="portfolio">
+            <div idPortfo="descriptionPortfolio" style="font-size:150%;">
+                <p>Présentateurs:<br/><br/></p>
+                <div style="display: flex;">
+                    <ul>
+                        <li> Gilane Barret (depuis janvier 2007)         </li>
+                        <li> Jean-Jacques Bourdin (depuis janvier 2007)  </li>
+                        <li> Éric Brunet (depuis janvier 2012)           </li>
+                        <li> Aurélie Casse (depuis décembre 2015)        </li>
+                        <li> Alice Darfeuille (depuis décembre 2018)     </li>
+                        <li> Christophe Delay (depuis juin 2007)         </li>
+                        <li> Benjamin Dubois (depuis mars 2013)          </li>
+                        <li> Roselyne Dubois (depuis juin 2007)          </li>
+                        <li> Florence Duprat (depuis novembre 2005)      </li>
+                        <li> Ruth Elkrief (depuis novembre 2005)         </li>
+                        <li> Stefan Etcheverry (depuis septembre 2018)   </li>
+                        <li> Adeline François (depuis août 2017)         </li>
+                        <li> Sandra Gandoin (depuis septembre 2011)      </li>
+                        <li> François Gapihan (depuis août 2012)         </li>
+                        <li> Philippe Gaudin (depuis juillet 2012)       </li>
+                    </ul>
+                    <ul>
+                        <li> Damien Gourlet (depuis août 2011)           </li>
+                        <li> Ronald Guintrange (depuis janvier 2007)     </li>
+                        <li> Sophie Hébrard (depuis juillet 2016)        </li>
+                        <li> Apolline de Malherbe (depuis septembre 2013)</li>
+                        <li> Alain Marschall (depuis août 2008)          </li>
+                        <li> Rachid M'Barki (depuis mai 2006)            </li>
+                        <li> Karine de Ménonville (depuis juin 2007)     </li>
+                        <li> Julien Migaud-Muller (depuis août 2013)     </li>
+                        <li> Thomas Misrachi (depuis novembre 2005)      </li>
+                        <li> Céline Moncel (depuis juillet 2013)         </li>
+                        <li> Laurent Neumann (depuis juillet 2014)       </li>
+                        <li> Céline Pitelet (depuis août 2011)           </li>
+                        <li> Dominique Rizet (depuis décembre 2012)      </li>
+                        <li> Perrine Storme (depuis juillet 2015)        </li>
+                        <li> Bruce Toussaint (depuis août 2018)          </li>
+                        <li> Olivier Truchot (depuis août 2008)          </li>
+                    </ul>
+                </div>
+                
+        </div>
+            <div id="imagePortfolio">
+                <img src="https://mvistatic.com/photosmvi/2019/08/06/P20736893D3794470G.jpg" alt="" srcset="">
+            </div>
+        </div>
+        <div id="contactUs">
+            <div id="map">
+                <img src="images/googleMap.png" alt="image de google Map représentant ou se situe BFMTV. Il se situe au 2 Rue du Général Alain de Boissieu, 75015 Paris" srcset="">
+            </div>
+            <div id="form">
+                <div style="display: flex;flex-flow: column;margin: auto;width: 50%;margin-top:10px;">
+                    <p style="margin-bottom:25px;">Nous Contacter:</p>
+                    <label for="object">Votre Nom</label>
+                    <input type="text" name="nom" id="nom">
+                    <label for="object">Votre adresse mail</label>
+                    <input type="text" name="nom" id="nom">
+                    <label for="object">Objet</label>
+                    <input type="text" name="objet" id="object">
+                    <label for="message">message</label>
+                    <textarea id="message" name="message" id="" cols="30" rows="10" style="resize:none;height: 200px;"></textarea>
+                    <input type="submit" value="Envoyer" style="with:100px;width: fit-content;margin: auto;">
+                </div>
+
+            </div>
+        </div>
+        <div id="footer">
+            <div id="footerPart1"> <p>Page crée par Pierre Sindin</p></div>
+            <div id="footerPart2"> <p>Pour la Formation Concepteur Devellopeur d'application</p> </div>
+            <div id="footerPart3"> <p>Derniere mise a jour le 02/10/2019</p> </div>
+        </div>
+
+
+    </div>
+    
+</body>
+</html>
